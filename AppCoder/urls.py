26299dfaf5ef082abc317d65f51171782dd0c70e
MohrGuillermo <mohrguillermo@outlook.com>
2022-06-15 00:17:07 -0300

    Todos los CRUDS añadidos

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,25 +1,42 @@
 from django.urls import path
-from .views import curso, formularioEntregable, formularioProfesor, inicio, entregable, estudiante, formularioEstudiante, cursoFormulario, buscar, busquedaDeComision, verProfesores, editarProfesor, eliminarProfesor
+from .views import curso, formularioEntregable, formularioProfesor, inicio, entregable, estudiante, formularioEstudiante, cursoFormulario, buscar, busquedaDeComision, verProfesores, editarProfesor, eliminarProfesor, eliminarCurso, editarCurso, buscarEstudiante, encontrarEstudiante, eliminarEstudiante, editarEstudiante, buscarProfesor, encontrarProfesor, buscarEntregable, editarEntregable, eliminarEntregable, encontrarEntregable
 
 # ESTE ES UN PASO DE VITAL IMPORTANCIA QUE PERMITIRA NOMBRAR CADA RUTA MEDIANTE LA CUAL ACCEDEREMOS A NUESTRAS VIEWS, TODA VIEW NECESITA TENER SU PATH PARA SER VISTA EN LA INTERFACE. 
 
 urlpatterns = [
     # paths de paginas iniciales
     path('', inicio, name='Inicio'),
+
+    # paths de paginas de cursos
     path('cursos/', curso, name= 'Cursos'),
-    path('Profesores/', verProfesores, name='Profesores'),
-    path('entregables/', entregable, name='Entregable'),
-    path('estudiantes/', estudiante, name='Estudiante'),
-    # paths de formularios
-    path('FormularioEstudiante/', formularioEstudiante, name='FormularioEstudiante'),
-    path('FormularioProfesores/', formularioProfesor, name='FormularioProfesores'),
+    path('editarCurso/<nombre>', editarCurso, name = 'editarCurso'),
+    path('eliminarCurso/<nombre>', eliminarCurso, name = 'eliminarCurso'),
     path('cursoFormulario/', cursoFormulario, name ='cursoFormulario'),
-    path('FormularioEntregable/', formularioEntregable, name = 'FormularioEntregable'),
-    # paths de busqueda
     path('BusquedaDeComision/', busquedaDeComision, name = 'BusquedaDeComision'	),
     path('Buscar/', buscar, name = 'Buscar'),
-    path('eliminarProfesor/<nombre>', eliminarProfesor, name = 'eliminarProfesor'),
-    path('editarProfesor/<profesor_nombre>', editarProfesor, name = 'editarProfesor'),
 
+    # paths de paginas de entregables
+    path('entregables/', entregable, name='Entregable'),
+    path('FormularioEntregable/', formularioEntregable, name = 'FormularioEntregable'),  
+    path('buscarEntregable/', buscarEntregable, name = 'buscarEntregable'	),
+    path('encontrarEntregable/', encontrarEntregable, name = 'encontrarEntregable'	),
+    path('eliminarEntregable/<nombre>', eliminarEntregable, name = 'eliminarEntregable'),
+    path('editarEntregable/<nombre>', editarEntregable, name = 'editarEntregable'),  
+    
+    # paths de paginas de estudiantes
+    path('estudiantes/', estudiante, name='Estudiante'),
+    path('FormularioEstudiante/', formularioEstudiante, name='FormularioEstudiante'),
+    path('buscarEstudiante/', buscarEstudiante, name = 'buscarEstudiante'	),
+    path('encontrarEstudiante/', encontrarEstudiante, name = 'encontrarEstudiante'	),
+    path('eliminarEstudiante/<nombre>', eliminarEstudiante, name = 'eliminarEstudiante'),
+    path('editarEstudiante/<nombre>', editarEstudiante, name = 'editarEstudiante'),
+
+    # paths de paginas de profesores
+    path('FormularioProfesores/', formularioProfesor, name='FormularioProfesores'),
+    path('editarProfesor/<profesor_nombre>', editarProfesor, name = 'editarProfesor'),
+    path('Profesores/', verProfesores, name='Profesores'),
+    path('eliminarProfesor/<nombre>', eliminarProfesor, name = 'eliminarProfesor'),
+    path('buscarProfesor/', buscarProfesor, name = 'buscarProfesor'	),
+    path('encontrarProfesor/', encontrarProfesor, name = 'encontrarProfesor'),
     
 ]   
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -14,6 +14,46 @@ def curso(request):
     lista_cursos = Curso.objects.all()
     return render(request, 'AppCoder/template_cursos.html', {'lista_cursos':lista_cursos})
 
+def busquedaDeComision(request):
+    return render(request, 'buscadorComision.html')
+
+def buscar(request):
+    if request.GET['comision']:
+        comision = request.GET['comision']
+        cursos = Curso.objects.filter(comision=comision)
+        return render(request, 'AppCoder/resultadosBusqueda.html', {'cursos':cursos, 'comision':comision},)
+    else:
+        respuesta = 'No se ingresó ningúna comisión'
+        return render(request, 'AppCoder/resultadosBusqueda.html', {'respuesta':respuesta})
+
+def eliminarCurso(request, nombre):
+    curso = Curso.objects.get(nombre=nombre)
+    curso.delete()
+    cursos = Curso.objects.all()
+    contexto = {'cursos':cursos}
+    return render(request, 'AppCoder/template_cursos.html', contexto)
+    
+def editarCurso(request, nombre):
+    curso = Curso.objects.get(nombre=nombre)
+
+    if request.method == 'POST':
+        miFormulario = CursoFormulario(request.POST)
+
+        if miFormulario.is_valid:
+            informacion = miFormulario.cleaned_data
+            curso.nombre = informacion['nombre']
+            curso.comision = informacion['comision']
+
+            curso.save()
+
+            return render(request, 'AppCoder/inicio.html')
+
+    else:
+        miFormulario = CursoFormulario(initial={'nombre':curso.nombre, 'comision':curso.comision})
+    
+    return render(request, 'editarCurso.html', {'miFormulario':miFormulario, 'nombre':nombre})
+            
+
 
 def cursoFormulario(request):
     
@@ -32,31 +72,11 @@ def cursoFormulario(request):
         miFormulario= CursoFormulario()
 
 
-# BUSQUEDA DE DATOS
-
-def busquedaDeComision(request):
-
-    return render(request, 'buscadorComision.html')
-
-
-def buscar(request):
-    
-    respuesta = f"Estoy buscando la comision Nº: { request.GET['comision'] }"
-    if request.GET['comision']:
-        comision = request.GET['comision']
-        cursos = Curso.objects.filter(comision=comision)
-        return render(request, 'AppCoder/resultadosBusqueda.html', {'cursos':cursos, 'comision':comision},)
-    else:
-        respuesta = 'No se ingresó ningúna comisión'
-        return render(request, 'AppCoder/resultadosBusqueda.html', {'respuesta':respuesta})
-    
-
-
-
 
 
 def estudiante(request):
-    return render(request, 'template_estudiantes.html')  
+    estudiantes = Estudiante.objects.all()
+    return render(request, 'template_estudiantes.html' , {'estudiantes':estudiantes})  
 # FORMULARIO COMUN
 # def formularioEstudiante(request):
 #     if request.method == 'POST':
@@ -82,9 +102,84 @@ def formularioEstudiante(request):
     return render(request, 'FormularioEstudiante.html', {'miFormulario':miFormulario})
 
 
+def buscarEstudiante(request):
+    return render(request, 'buscarEstudiante.html')
+
+def encontrarEstudiante(request):
+    if request.GET['nombre']:
+        nombre = request.GET['nombre']
+        estudiantes = Estudiante.objects.filter(nombre=nombre)
+        return render(request, 'AppCoder/resultadosEstudiante.html', {'nombre':nombre, 'estudiantes':estudiantes})
+    else:
+        respuesta = 'No se ingresó ningún estudiante con ese nombre'
+        return render(request, 'AppCoder/resultadosEstudiante.html', {'respuesta':respuesta})
+
+def eliminarEstudiante(request, nombre):
+    estudiante = Estudiante.objects.get(nombre=nombre)
+    estudiante.delete()
+    estudiantes = Estudiante.objects.all()
+    contexto = {'estudiantes':estudiantes}
+    return render(request, 'AppCoder/template_estudiantes.html', contexto)
+
+def editarEstudiante(request, nombre):
+    estudiante = Estudiante.objects.get(nombre=nombre)
+
+    if request.method == 'POST':
+        miFormulario = EstudianteFormulario(request.POST)
+
+        if miFormulario.is_valid:
+            informacion = miFormulario.cleaned_data
+            estudiante.nombre = informacion['nombre']
+            estudiante.apellido = informacion['apellido']
+            estudiante.email = informacion['email']
+
+            estudiante.save()
+
+            return render(request, 'AppCoder/inicio.html')
+
+    else:
+        miFormulario = EstudianteFormulario(initial={'nombre':estudiante.nombre, 'apellido':estudiante.apellido, 'email':estudiante.email})
+    
+    return render(request, 'editarEstudiante.html', {'miFormulario':miFormulario, 'nombre':nombre})
+
+
+# def eliminarCurso(request, nombre):
+#     curso = Curso.objects.get(nombre=nombre)
+#     curso.delete()
+#     cursos = Curso.objects.all()
+#     contexto = {'cursos':cursos}
+#     return render(request, 'AppCoder/template_cursos.html', contexto)
+    
+# def editarCurso(request, nombre):
+#     curso = Curso.objects.get(nombre=nombre)
+
+#     if request.method == 'POST':
+#         miFormulario = CursoFormulario(request.POST)
+
+#         if miFormulario.is_valid:
+#             informacion = miFormulario.cleaned_data
+#             curso.nombre = informacion['nombre']
+#             curso.comision = informacion['comision']
+
+#             curso.save()
+
+#             return render(request, 'AppCoder/inicio.html')
+
+#     else:
+#         miFormulario = CursoFormulario(initial={'nombre':curso.nombre, 'comision':curso.comision})
+    
+#     return render(request, 'editarCurso.html', {'miFormulario':miFormulario, 'nombre':nombre})
+            
+    
+
+
+
+
+
 
 def entregable(request):
-    return render(request, 'template_entregables.html'  )
+    entregables = Entregable.objects.all()
+    return render(request, 'template_entregables.html', {'entregables':entregables})
 
 def formularioEntregable(request):
     if request.method == 'POST':
@@ -100,6 +195,46 @@ def formularioEntregable(request):
     else:
         miFormulario = EntregableFormulario()    
     return render(request, 'FormularioEntregable.html', {'miFormulario':miFormulario})
+
+def eliminarEntregable(request, nombre):
+    entregable = Entregable.objects.get(nombre=nombre)
+    entregable.delete()
+    entregables = Entregable.objects.all()
+    contexto = {'entregables':entregables}
+    return render(request, 'AppCoder/template_entregables.html', contexto)
+
+
+def editarEntregable (request, entregable_nombre):
+    entregable = Entregable.objects.get(nombre=entregable_nombre)
+    if request.method == 'POST':
+        miFormulario = EntregableFormulario(request.POST)
+
+        if miFormulario.is_valid(): # Si el formulario es valido, se guarda.
+            informacion = miFormulario.cleaned_data
+            entregable.nombre = informacion['nombre']
+            entregable.fecha_de_entrega = informacion['fecha_de_entrega']
+            entregable.entregado = informacion['entregado']
+            entregable.save()
+
+            return render(request, 'AppCoder/inicio.html') 
+
+    else:
+        miFormulario = EntregableFormulario(initial={'nombre':entregable.nombre, 'apellido':entregable.apellido, 'email':entregable.email, 'profesion':entregable.profesion})
+    # Se renderiza la pagina de edicion.
+    return render(request, 'editarEntregable.html', {'miFormulario':miFormulario, 'entregable_nombre':entregable_nombre})
+
+
+def buscarEntregable(request):
+    return render(request, 'buscarEntregable.html')
+
+def encontrarEntregable(request):
+    if request.GET['nombre']:
+        nombre = request.GET['nombre']
+        entregables = Entregable.objects.filter(nombre=nombre)
+        return render(request, 'AppCoder/resultadosEntregable.html', {'nombre':nombre, 'entregables':entregables})
+    else:
+        respuesta = 'No se ingresó ningún entregable con ese nombre'
+        return render(request, 'AppCoder/resultadosEntregable.html', {'respuesta':respuesta})
 
 
    
@@ -154,10 +289,22 @@ def editarProfesor (request, profesor_nombre):
             profesor.save()
 
 
-            return render(request, 'AppCoder/template_profesores.html') # Se redirige a la pagina de inicio.
+            return render(request, 'AppCoder/inicio.html') # Se redirige a la pagina de inicio.
     # Si el metodo es GET, se muestra el formulario.
     else:
         miFormulario = ProfesorFormulario(initial={'nombre':profesor.nombre, 'apellido':profesor.apellido, 'email':profesor.email, 'profesion':profesor.profesion})
     # Se renderiza la pagina de edicion.
     return render(request, 'editarProfesor.html', {'miFormulario':miFormulario, 'profesor_nombre':profesor_nombre})
 
+
+def buscarProfesor(request):
+    return render(request, 'buscarProfesor.html')
+
+def encontrarProfesor(request):
+    if request.GET['nombre']:
+        nombre = request.GET['nombre']
+        profesores = Profesor.objects.filter(nombre=nombre)
+        return render(request, 'AppCoder/resultadosProfesor.html', {'nombre':nombre, 'profesores':profesores})
+    else:
+        respuesta = 'No se ingresó ningún profesor con ese nombre'
+        return render(request, 'AppCoder/resultadosProfesor.html', {'respuesta':respuesta})
